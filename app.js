e97af2b9ff5f10d647689550d7e76938e3f12a34
A.J.2M <anthyjosemolina@gmail.com>
2024-03-03 13:45:05 -0500

    Se organiza logica, comentarea codigo y eliminan dependencias no usadas

--- app.js
+++ app.js
@@ -1,52 +1,84 @@
+// Importa el módulo tensorflow para Node.js
 import "@tensorflow/tfjs-node";
+// Importa face-api.js que proporciona herramientas para el reconocimiento facial
 import * as faceapi from "@vladmandic/face-api";
+// Importa el módulo fs para interactuar con el sistema de archivos
+import * as fs from "fs";
+// Importa el módulo path para manejar rutas de archivos
+import * as path from "path";
+// Importa el módulo canvas, que permite manipular imágenes en el servidor
 import * as canvas from "canvas";
 
+// Extrae las clases Canvas, Image, y ImageData del módulo canvas
 const { Canvas, Image, ImageData } = canvas;
+// Sobrescribe el entorno para que las funciones de face-api.js funcionen con Canvas, Image y ImageData
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
+// Define una función asincrónica llamada 'run' para realizar el proceso de reconocimiento facial
 async function run() {
-  // Cargar modelos pre-entrenados
+  // Carga los modelos necesarios para el reconocimiento facial desde el disco
   await faceapi.nets.faceRecognitionNet.loadFromDisk("./models");
   await faceapi.nets.faceLandmark68Net.loadFromDisk("./models");
   await faceapi.nets.ssdMobilenetv1.loadFromDisk("./models");
 
-  const savedImg = "guardada3.jpeg";
-  const queryImg = "consulta.jpeg";
-  const savedCanva = await canvas.loadImage(savedImg);
-  const queryCanva = await canvas.loadImage(queryImg);
+  // Directorio donde se almacenan las imágenes de referencia
+  const savedDir = "./db_fotos";
+  // Nombre del archivo de imagen a consultar
+  const queryImg = "consulta.jpg";
+  // Carga la imagen de consulta en un objeto Canvas
+  const queryCanvas = await canvas.loadImage(queryImg);
 
-  // Detectar rostros y landmarks en la imagen guardada
-  const resultSaved = await faceapi
-    .detectAllFaces(savedCanva)
-    .withFaceLandmarks()
-    .withFaceDescriptors();
-
-  // Detectar un solo rostro y landmarks en la imagen de consulta
+  // Detecta la cara en la imagen de consulta y extrae los puntos característicos y descripciones faciales
   const resultsQuery = await faceapi
-    .detectSingleFace(queryCanva)
-    .withFaceLandmarks()
-    .withFaceDescriptor();
+    .detectSingleFace(queryCanvas) // Detecta una sola cara en la imagen
+    .withFaceLandmarks() // Extrae los puntos característicos de la cara
+    .withFaceDescriptor(); // Extrae la descripción facial de la cara
 
-  // Verificar si se detectaron rostros en alguna de las imágenes
-  if (resultSaved.length === 0 || !resultsQuery) {
-    console.log("No se detectaron rostros en una de las imágenes.");
-    return;
-  }
+  // Crea un objeto FaceMatcher para comparar las caras detectadas con las caras almacenadas
+  const faceMatcher = new faceapi.FaceMatcher(resultsQuery);
 
-  // Crear un FaceMatcher con los rostros de la imagen guardada
-  const faceMatcher = new faceapi.FaceMatcher(resultSaved);
+  // Imprime un mensaje indicando que se está buscando la persona en las imágenes guardadas...
+  console.log("Buscando persona en las imágenes guardadas...");
 
-  // Encontrar la mejor coincidencia de rostros en la imagen de consulta
-  const bestMatch = faceMatcher.findBestMatch(resultsQuery.descriptor);
+  // Lee el directorio donde se almacenan las imágenes de referencia
+  fs.readdir(savedDir, async (err, files) => {
+    if (err) {
+      // Imprime un mensaje de error si no se puede leer el directorio
+      console.error("Error al leer el directorio:", err);
+      return;
+    }
 
-  console.log(bestMatch);
+    // Itera sobre cada archivo en el directorio de imágenes guardadas
+    for (const file of files) {
+      // Construye la ruta completa del archivo
+      const filePath = path.join(savedDir, file);
+      // Carga la imagen guardada en un objeto Canvas
+      const savedCanvas = await canvas.loadImage(filePath);
+      // Detecta todas las caras en la imagen guardada y extrae sus puntos característicos y descripciones faciales
+      const resultSaved = await faceapi
+        .detectAllFaces(savedCanvas) // Detecta todas las caras en la imagen
+        .withFaceLandmarks() // Extrae los puntos característicos de las caras
+        .withFaceDescriptors(); // Extrae las descripciones faciales de las caras
 
-  if (bestMatch.label !== "unknown") {
-    console.log(`La persona existe en la imagen guardada: ${bestMatch.label}`);
-  } else {
-    console.log("La persona no fue encontrada en la imagen guardada.");
-  }
+      // Comprueba si se encontraron caras en la imagen guardada y en la imagen de consulta
+      if (resultSaved.length > 0 && resultsQuery) {
+        // Itera sobre cada resultado de cara detectada en la imagen guardada
+        resultSaved.forEach((fd) => {
+          // Encuentra la mejor coincidencia entre la cara detectada en la imagen guardada y la cara de la imagen de consulta
+          const bestMatch = faceMatcher.findBestMatch(fd.descriptor);
+          // Comprueba si la coincidencia tiene una etiqueta diferente a "unknown"
+          if (bestMatch.label !== "unknown") {
+            // Imprime un mensaje indicando que la persona existe en la imagen guardada
+            console.log(`La persona existe en la imagen: ${file}`);
+          } else {
+            // Imprime un mensaje indicando que la persona no existe en la imagen guardada
+            console.log(`La persona NO existe en la imagen: ${file}`);
+          }
+        });
+      }
+    }
+  });
 }
 
+// Llama a la función 'run' para iniciar el proceso de reconocimiento facial
 run();
